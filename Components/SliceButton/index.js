0065b2da3c7fa47f37f06fd4144af157a26027bb
reggie_anim reggie_anim <reggieanim@gmail.com>
2020-07-24 02:01:57 +0000

    Updates

---
+++ Components/SliceButton/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _SliceButton = require("./SliceButton");
+
+var _SliceButton2 = _interopRequireDefault(_SliceButton);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _SliceButton2.default;
